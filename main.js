275a6f0f6aff81923c3e7cf11bf23189a4fcb0b4
IsaacDJohnson <isaac.designws@gmail.com>
2023-06-23 14:51:15 -0500

    completed

--- main.js
+++ main.js
@@ -4,8 +4,32 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const buttons = document.querySelectorAll(".like-glyph")
 
+function clickFunction(e){
+  const heart = e.target
 
+  mimicServerCall("URL")
+  .then(() => {
+    if (heart.innerText === EMPTY_HEART){
+      heart.innerText = FULL_HEART;
+      heart.className = "activate-heart";
+    } else { 
+      heart.innerText = EMPTY_HEART;
+      heart.className = "";
+    }
+  })
+  .catch((error)=>{
+    const modal = document.getElementById('modal');
+    modal.className = "";
+    modal.innerText = error;
+    setTimeout(() => modal.className = 'hidden', 3000);
+  });
+}
+
+for(const glyph of buttons){
+  glyph.addEventListener("click", clickFunction)
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
@@ -18,8 +42,11 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       if (isRandomFailure) {
         reject("Random server error. Try again.");
       } else {
-        resolve("Pretend remote server notified of action!");
+        resolve("Pretend remote server notified of action!"); 
       }
     }, 300);
   });
 }
+
+
+
